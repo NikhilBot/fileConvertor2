7119ffe7b4c2a2d0810eb48ee6955c072efbad0c
NikhilBot <nikhilkushwaha44@gmail.com>
2020-06-25 14:40:04 +0530

    initial commit2

---
+++ src/main/java/com/manifesting/fileConvertor/FileConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.manifesting.fileConvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manifesting/fileConvertor/FileConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifesting.fileConvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
